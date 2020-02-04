14724ed822d603cdbf44d78eecf75900886e8ead
whatTHEh3ll <47017391+whatTHEh3ll@users.noreply.github.com>
2020-02-04 12:23:28 -0800

    Update main.tf

--- AZURE-terraform/kube_code/main.tf
+++ AZURE-terraform/kube_code/main.tf
@@ -15,7 +15,7 @@ module "aks" {
   source  = "Azure/aks/azurerm"
   version = "2.0.0"
 
-  CLIENT_ID = "f031ce1d-a362-4cd3-b4d6-e0826fffaefd"
-  CLIENT_SECRET = "727af339-49bd-4780-a5a5-95fb2a295d5b"
+  CLIENT_ID = ""
+  CLIENT_SECRET = ""
   prefix = "your-custom-resource-prefix"
 }
